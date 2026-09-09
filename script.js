781fe3999594f9a26d9abb0ee53b2f3fe1bc95dc
Ebi Emmerich-Adehor <ebw24@gmail.com>
2026-09-09 09:16:48 +0200

    updated professional title and greeting

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Typed.js Initialization
     if (document.querySelector('.typed-text')) {
         var typed = new Typed('.typed-text', {
-            strings: ["Work Remotely", "Web Developer", "AI and Software Engineering professional"],
+            strings: ["I Work Remotely", "Web Developer", "AI and Software Engineering professional"],
             typeSpeed: 50,
             backSpeed: 30,
             backDelay: 1500,
